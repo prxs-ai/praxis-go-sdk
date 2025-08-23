821f6950f761281934fe4a7902ec86d3e93cc8f4
hyp0cr4t <beaumont121990@gmail.com>
2025-08-23 15:16:19 +0000

    chore: :rocket: add release-please support

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.1.0 (Initial Release)
+
+Initial release of go-p2p-agent.
---
+++ version.go
@@ -0,0 +1,4 @@
+package main
+
+// Version is the current release version of go-p2p-agent
+const Version = "0.1.0"
